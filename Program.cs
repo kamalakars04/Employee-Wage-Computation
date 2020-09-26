ce335a958ac3e18067926ceca853b43df578bbaa
kamalakars04 <kamalakars04@gmail.com>
2020-09-26 21:23:53 +0530

    Adding empcheck UC1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,21 @@ namespace Employee_Wage_Computation1
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            Random r = new Random();
+            int random_Num = r.Next(0, 2);
+            string emp_Check = "";
+            if(random_Num==1)
+            {
+                emp_Check = "Present";
+                Console.WriteLine("Employee is {0}", emp_Check);
+            }
+            else
+            {
+                emp_Check = "Absent";
+                Console.WriteLine("Employee is {0}", emp_Check);
+            }
+                
         }
     }
 }
