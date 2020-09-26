3155a6aefada3c38ff7247d9451823676486af45
kamalakars04 <kamalakars04@gmail.com>
2020-09-26 20:50:01 +0530

    Adding Master Branch

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Wage_Computation1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
