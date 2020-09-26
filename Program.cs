526b920afbc63ac42155a81681ccf7175654760d
kamalakars04 <kamalakars04@gmail.com>
2020-09-26 21:31:59 +0530

    Added daily wage UC2

--- Program.cs
+++ Program.cs
@@ -11,10 +11,14 @@ namespace Employee_Wage_Computation1
             Random r = new Random();
             int random_Num = r.Next(0, 2);
             string emp_Check = "";
-            if(random_Num==1)
+            int f_T_WorkingHours = 8;
+            int wagePerHour = 20;
+            if (random_Num==1)
             {
                 emp_Check = "Present";
                 Console.WriteLine("Employee is {0}", emp_Check);
+                Console.WriteLine("Daily wage of employee is {0}",f_T_WorkingHours*wagePerHour);
+
             }
             else
             {
