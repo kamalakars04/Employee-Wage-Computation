b4726318f728b2ea5c1fe4304376d6b8b130cffc
kamalakars04 <kamalakars04@gmail.com>
2020-09-29 12:24:30 +0530

    [Kamalakar].[Refactor].Adding UC7 Using class Methods

--- Program.cs
+++ Program.cs
@@ -4,66 +4,76 @@ namespace Employee_Wage_Computation1
 {
     class Program
     {
+        // constants
+        const int WAGE_PER_HOUR = 20;
+        const int MAX_WORKDAYS_IN_MONTH = 20;
+        const int MAX_WORK_HOURS = 100;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
 
-            Random r = new Random();
-            int random_Num = r.Next(0, 3);
-            string emp_Check = "";
-            int f_T_WorkingHours = 8;
-            int p_T_WorkingHours = 4;
-            int wagePerHour = 20;
-            int work_Days_In_Month = 20;
-            int total_Emp_Work_Hours=0;
-            int monthly_Wage;
+            Program program = new Program();
+            program.CalculateTotalWage();
 
-
-                switch (random_Num)
-                {
-                    case 1:
-
-                        emp_Check = "Present";
-                        Console.WriteLine("Employee is {0} and full time", emp_Check);
-                        Console.WriteLine("Daily wage of employee is {0}",f_T_WorkingHours*wagePerHour);
-                        break;
-                
-                    case 2:
-
-                        emp_Check = "Present";
-                        Console.WriteLine("Employee is {0} and Part time", emp_Check);
-                        Console.WriteLine("Daily wage of employee is {0}", p_T_WorkingHours * wagePerHour);
-                        break;
-
-                    case 0:
-
-                        emp_Check = "Absent";
-                        Console.WriteLine("Employee is {0}", emp_Check);
-                        break;
-                }
-            for(int i=1; i<=work_Days_In_Month;i++)
-            {
-                switch (r.Next(0, 3))
-                {
-                    case 1:
-                        total_Emp_Work_Hours += 8;//emp present for full day on that working day
-                        break;
-                    case 2:
-                        total_Emp_Work_Hours += 4; //emp present for part time on that working day
-                        break;
-                    case 0:
-                        break;
-                }
-            }
-
-            Console.WriteLine("Maximum no of working hours to be considered is 100");
-            Console.WriteLine("No of working hours of employee in a month are {0}", total_Emp_Work_Hours);
-            if (total_Emp_Work_Hours <= 100)
-                monthly_Wage = total_Emp_Work_Hours * 20;
-            else
-                monthly_Wage = 100 * 20;
-
-            Console.WriteLine("Montly wage of employee is {0}",monthly_Wage);
         }
+
+        public void CalculateTotalWage() //Method to calculate total wage
+        {
+            //variables
+            int dailyWage = 0;
+            int dailyEmpHours = 0;
+            int numberOfWorkDays = 0;
+            int totalEmpWorkHours = 0;
+            int totalMonthlyWage;
+            while(totalEmpWorkHours < MAX_WORK_HOURS && numberOfWorkDays < MAX_WORKDAYS_IN_MONTH )
+            {
+                numberOfWorkDays++;                         // Calculates No of working days till now in month
+                dailyEmpHours = CalculateDailyEmpHours();   // Calculates No of working hours daily
+                dailyWage = dailyEmpHours * WAGE_PER_HOUR;
+                Console.WriteLine("The daily Wage of employee on day {0} is {1}", numberOfWorkDays , dailyWage);
+
+                // calculates total working hours
+                if (totalEmpWorkHours + dailyEmpHours <= MAX_WORK_HOURS)
+                    totalEmpWorkHours += dailyEmpHours;
+                else
+                    totalEmpWorkHours = MAX_WORK_HOURS;
+            }
+            totalMonthlyWage = totalEmpWorkHours * WAGE_PER_HOUR;
+            Console.WriteLine("The monthly wage of employee is {0}", totalMonthlyWage );
+           
+
+        }
+
+        public int CalculateDailyEmpHours() //Method to calculate daily work hours of employee
+        {
+            // constants
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            
+            //variables
+            int empHours = 0;
+            Random random = new Random();
+            int workType = random.Next(0, 3);
+            switch (workType)
+            {
+                case IS_FULL_TIME:
+
+                    empHours = 8;       //emp present for full day on that working day
+                    break;
+
+                case IS_PART_TIME:
+
+                    empHours = 4;      //emp present for part time on that working day
+                    break;
+
+                default:
+
+                    empHours = 0;
+                    break;
+            }
+            return empHours;
+        }
+        
     }
 }
